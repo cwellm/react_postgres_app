747583ae9c33419549fdbdfe004e4c41d89b96b3
cwellm <christoph.wellm@creaflect.de>
2025-06-28 21:13:26 +0200

    Backup 2025_06_28

--- db_scripts/analytic_functions.sql
+++ db_scripts/analytic_functions.sql
@@ -1,16 +1,10 @@
 DROP FUNCTION IF EXISTS api.get_total_country_analytics_data;
 CREATE OR REPLACE FUNCTION api.get_total_country_analytics_data()
-
-CREATE OR REPLACE FUNCTION api.get_all_addresses()
-       RETURNS SETOF public.user_stored_addresses
-       AS $$
-BEGIN
-RETURN QUERY
-SELECT * FROM public.user_stored_addresses;
-IF NOT FOUND THEN
-                RAISE EXCEPTION 'No addresses found.';
-END IF;
-            RETURN;
+    RETURNS TABLE (_name_ VARCHAR(255),_iso3_ CHAR(3),_cnt_ BIGINT)
+    AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT name,iso3,COUNT(*) FROM country_codes a INNER JOIN user_stored_addresses b ON a.iso3 = b.country GROUP BY a.iso3,a.name;
+        RETURN;
 END;
-       $$ LANGUAGE plpgsql
-;
+    $$ LANGUAGE plpgsql
--- src/boards/countryPieChart.js
+++ src/boards/countryPieChart.js
@@ -1,30 +1,44 @@
 import 'devextreme/dist/css/dx.light.css';
 
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PieChart, {Label, Series, Tooltip} from 'devextreme-react/pie-chart';
 import Button from 'devextreme-react/button';
 
 import { useNavigate } from 'react-router';
 
-
-const countryData = [
-    {
-        country: 'China',
-        numberOfAddressesWithCountry: 1002
-    },
-    {
-        country: 'United States',
-        numberOfAddressesWithCountry: 716
-    }
-]
+async function fetchCountryAnalyticsData(){
+    return fetch('http://localhost:3001/rpc/get_total_country_analytics_data', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    }).then(res => {
+        if (res.status !== 200) return null;
+        return res.json();
+    }).catch((err) => {
+        alert(`Error fetching country analytics data: ${err}`);
+        return null;
+    });
+}
 
 const customizeText = (countryData) => {
     return countryData.value + " addresses";
 }
 
 function CountryPieChart() {
+    const [countryAnalyticsData, setCountryAnalyticsData] = useState(null);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const loadCountryAnalyticsData = async () => {
+            let countryAnalyticsData_ = await fetchCountryAnalyticsData();
+            setCountryAnalyticsData(countryAnalyticsData_);
+        }
+        loadCountryAnalyticsData();
+    }, []);
+
     return (
         <div>
             <Button
@@ -38,12 +52,12 @@ function CountryPieChart() {
         <PieChart
             id = "country-pie-chart"
             type="doughnut"
-            dataSource={countryData}
+            dataSource={countryAnalyticsData}
             title="Number of Addresses by Country"
         >
             <Series
-                argumentField="country"
-                valueField="numberOfAddressesWithCountry"
+                argumentField="_name_"
+                valueField="_cnt_"
                 >
                 <Label
                     visible={true}
@@ -51,10 +65,17 @@ function CountryPieChart() {
                     customizeText={customizeText}
                 />
             </Series>
-            <Tooltip enabled={true} />
+            <Tooltip
+                enabled={true}
+                customizeTooltip={customizeTooltip}
+            />
         </PieChart>
         </div>
     );
+}
+
+function customizeTooltip(pointInfo) {
+    return {text: pointInfo.argument};
 }
 
 export default CountryPieChart;
